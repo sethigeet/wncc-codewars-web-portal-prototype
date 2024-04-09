b197a5f61b5ef3404e10670e2b9f619920019ebf
Geet Sethi <geetsethi05@gmail.com>
2024-04-09 16:12:22 +0530

    initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="bg-green-400">
+      <h1>Hello from tailwind</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+}
